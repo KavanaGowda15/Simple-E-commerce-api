2207e033d00a383bb31fee82e487ff5c67c0e5fe
Kavana D <kavanasahana15@gmail.com>
2025-07-16 13:25:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
